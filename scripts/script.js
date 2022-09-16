2a3fe2c25df9ae3f0288ef2fa6c772af6f6a9f5c
Yana Morozova <yanabonne@yandex.ru>
2022-09-16 16:05:23 +0300

    fix: исправить файловую структуру и откорректировать согласно правкам

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+let penButton = document.querySelector('.profile__edit');
+let crossButton = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let popupName = document.querySelector('.popup__text_name');
+let popupDescription = document.querySelector('.popup__text_description');
+let popupForm = document.querySelector('.popup__container');
+
+function popupOpen() {
+    popup.classList.add('popup_opened');
+    popupName.value = profileName.textContent;
+    popupDescription.value = profileDescription.textContent;
+}
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+function changeInfo(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = popupName.value;
+    profileDescription.textContent = popupDescription.value;
+
+    popupClose();
+}
+
+penButton.addEventListener('click', popupOpen);
+crossButton.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', changeInfo);
+
+/* let heart = document.querySelector('.photo-grid__like');
+
+function like() {
+    if (heart.classList.contains('photo-grid__like_active') === false) {
+        heart.src = 'images/like-button_black.svg';
+        heart.classList.add('photo-grid__like_active');
+    }
+    else {
+        heart.src = 'images/like-button.svg';
+        heart.classList.remove('photo-grid__like_active');
+    }
+}
+
+heart.addEventListener('click', like); */
